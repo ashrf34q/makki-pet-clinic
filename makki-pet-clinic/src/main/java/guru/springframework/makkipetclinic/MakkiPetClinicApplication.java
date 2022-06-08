96c04c44acea357bddf2917b5988008144f6aed3
Ashraf Makki <makkiashraf08@gmail.com>
2022-06-07 22:25:59 -0400

    Initial source files and properties

---
+++ makki-pet-clinic/src/main/java/guru/springframework/makkipetclinic/MakkiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.makkipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakkiPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakkiPetClinicApplication.class, args);
+	}
+
+}
