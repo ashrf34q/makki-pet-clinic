e4f493b0dd267068a1bc35da37f35b20944592fb
Ashraf Makki <makkiashraf08@gmail.com>
2022-06-27 19:38:40 -0400

    refactored map impl to auto ID generation. closes #24

--- makki-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/makkipetclinic/services/map/AbstractMapService.java
+++ makki-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/makkipetclinic/services/map/AbstractMapService.java
@@ -1,15 +1,19 @@
 package guru.springframework.makkipetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
+
+import guru.springframework.makkipetclinic.model.BaseEntity;
 
 //this abstract class will be used to implement CrudService interface methods.
 
-public abstract class AbstractMapService<T, ID> {	// An abstract class is a restricted class that annot be used to 
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {	// An abstract class is a restricted class that annot be used to 
 													//create objects(to access it it must be inherited from another class)
-	protected Map<ID,T> map = new HashMap<>();
+	protected Map<Long,T> map = new HashMap<>();
 	
 	Set<T> findAll(){
 		return new HashSet<>(map.values());	//return everything in the map
@@ -19,17 +23,35 @@ public abstract class AbstractMapService<T, ID> {	// An abstract class is a rest
 		return map.get(id);
 	}
 	
-	T save(ID id, T object) {
-		map.put(id, object);
+	T save(T object) {
+		if(object != null) {
+			if(object.getId() == null) {
+				object.setId(getNextID());
+			}
+			map.put(object.getId(), object);
+		} else {
+		throw new RuntimeException("Object cannot be null!");	
+		}
 		return object;
 	}
-	
+	 
 	void deleteById(ID id) {
 		map.remove(id);
 	}
 	
 	void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+	
+	private Long getNextID() {
+		Long nextId = null;
+		
+		try {
+			nextId = Collections.max(map.keySet()) + 1;
+		} catch (NoSuchElementException e) {
+			nextId = 1L;
+		}
+		return nextId;
 	}
 
 }
--- makki-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/makkipetclinic/bootstrap/DataLoader.java
+++ makki-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/makkipetclinic/bootstrap/DataLoader.java
@@ -22,7 +22,6 @@ public class DataLoader implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
 		owner1.setFirstName("Tommy");
 		owner1.setLastName("Shelby");
 		
@@ -31,7 +30,6 @@ public class DataLoader implements CommandLineRunner{
 		System.out.println("Loaded owners");
 		
 		Vet vet1 = new Vet();
-		vet1.setId(1L);
 		vet1.setFirstName("Polly");
 		vet1.setLastName("Gray");
 	
