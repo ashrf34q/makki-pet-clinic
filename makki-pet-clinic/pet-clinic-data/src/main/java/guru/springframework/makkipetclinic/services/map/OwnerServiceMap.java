b844130017071de1ff2dd948fa76c35374296d29
Ashraf Makki <makkiashraf08@gmail.com>
2022-07-04 22:27:47 -0400

    Added Pets and Contact info on startup. closes #34

--- makki-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/makkipetclinic/services/map/OwnerServiceMap.java
+++ makki-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/makkipetclinic/services/map/OwnerServiceMap.java
@@ -5,14 +5,48 @@ import java.util.Set;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.makkipetclinic.model.Owner;
+import guru.springframework.makkipetclinic.model.Pet;
 import guru.springframework.makkipetclinic.services.OwnerService;
+import guru.springframework.makkipetclinic.services.PetService;
+import guru.springframework.makkipetclinic.services.PetTypeService;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+	
+	private final PetTypeService petTypeService;
+	private final PetService petService;
 
+	public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+		this.petTypeService = petTypeService;
+		this.petService = petService;
+	}
+
+	
 	@Override
 	public Owner save(Owner object) {
+		if(object != null) {
+			if(object.getPets() != null) {
+				object.getPets().forEach(pet -> {
+					if(pet.getPetType() != null) {
+						if(pet.getPetType().getId() == null) { 
+							pet.setPetType(petTypeService.save(pet.getPetType()));
+						}
+					}
+					else {
+						throw new RuntimeException("Pet Type is required");
+					}
+					
+					if(pet.getId() == null) {
+						Pet savedPet = petService.save(pet);
+						pet.setId(savedPet.getId());
+					}
+				});
+			}
 		return super.save(object);
+	   }
+		else {
+			return null;
+		}
 	}
 
 	@Override
--- makki-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/makkipetclinic/bootstrap/DataLoader.java
+++ makki-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/makkipetclinic/bootstrap/DataLoader.java
@@ -1,9 +1,12 @@
 package guru.springframework.makkipetclinic.bootstrap;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import guru.springframework.makkipetclinic.model.Owner;
+import guru.springframework.makkipetclinic.model.Pet;
 import guru.springframework.makkipetclinic.model.PetType;
 import guru.springframework.makkipetclinic.model.Vet;
 import guru.springframework.makkipetclinic.services.OwnerService;
@@ -27,18 +30,48 @@ public class DataLoader implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		
 		PetType cat = new PetType();
-		cat.setName("Baby");
+		cat.setName("cat");
 		PetType savedCatType = petTypeService.save(cat);
 		
 		PetType dog = new PetType();
-		dog.setName("Obi-Wan");
-		PetType savedPetType = petTypeService.save(dog);
+		dog.setName("dog");
+		PetType savedDogPetType = petTypeService.save(dog);
+		
+		PetType horse = new PetType();
+		horse.setName("horse");
+		PetType savedHorsePetType = petTypeService.save(horse);
 		
 		Owner owner1 = new Owner();
-		owner1.setFirstName("Tommy");
+		owner1.setFirstName("Thomas");
 		owner1.setLastName("Shelby");
+		owner1.setAddress("1832 Brickell");
+		owner1.setCity("Birmingham");
+		owner1.setPhone("3132340121");
+		
+		Pet TommysPet = new Pet();
+		TommysPet.setName("Grace's Secret");
+		TommysPet.setPetType(savedHorsePetType);
+		TommysPet.setOwner(owner1);
+		TommysPet.setBirthDate(LocalDate.of(1910, 04, 14));
+		owner1.getPets().add(TommysPet);
 		
 		ownerService.save(owner1);
+		
+		Owner owner2 = new Owner();
+		owner2.setFirstName("Arthur");
+		owner2.setLastName("Shelby");
+		owner2.setAddress("1832 Brickell");
+		owner2.setCity("Birmingham");
+		owner2.setPhone("3133345681");
+		
+		Pet arthursPet = new Pet();
+		arthursPet.setName("Baby");
+		arthursPet.setPetType(savedCatType);
+		arthursPet.setBirthDate(LocalDate.of(1919, 07, 04));
+		arthursPet.setOwner(owner2);
+		owner2.getPets().add(arthursPet);
+		
+		ownerService.save(owner2);
 		
 		System.out.println("Loaded owners");
 		
