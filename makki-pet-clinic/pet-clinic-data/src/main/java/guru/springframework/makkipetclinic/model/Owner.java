cce31685a0cd6ce3e8ce98e2d0fc57d7b74e1e72
Ashraf Makki <makkiashraf08@gmail.com>
2022-07-13 13:45:54 -0400

    Converted owners to JPA entities. closes #37

--- makki-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/makkipetclinic/model/Owner.java
+++ makki-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/makkipetclinic/model/Owner.java
@@ -3,11 +3,26 @@ package guru.springframework.makkipetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 	
+	@Column(name = "address")
 	private String address;
+	
+	@Column(name = "city")
 	private String city;
+	
+	@Column(name = "phone")
 	private String phone;
+
+	@OneToMany(cascade = CascadeType.ALL)
 	private Set<Pet> pets = new HashSet<>() ;
 	
 	public String getAddress() {
--- makki-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/makkipetclinic/model/Person.java
+++ makki-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/makkipetclinic/model/Person.java
@@ -1,8 +1,14 @@
 package guru.springframework.makkipetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
-	
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
 	
 	public String getFirstName() {
@@ -17,4 +23,5 @@ public class Person extends BaseEntity {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}	
+	
 }
