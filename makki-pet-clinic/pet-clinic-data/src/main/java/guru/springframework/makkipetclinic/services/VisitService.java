f972c0d095eaedbb402a72ca20dbc3e334fe04d9
Ashraf Makki <makkiashraf08@gmail.com>
2022-07-14 21:34:17 -0400

    Implemented Visit Map Service. closes #47

---
+++ makki-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/makkipetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.makkipetclinic.services;
+
+import guru.springframework.makkipetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
